7b8b78197ff89a4eaa06165147b3294a2c347413
Shweta Landage <shwetalandageraut@gmail.com>
2026-04-25 11:09:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+sections.forEach(sec => {
+  sec.style.opacity = "0";
+  sec.style.transform = "translateY(40px)";
+  sec.style.transition = "0.6s";
+  observer.observe(sec);
+});
+
+function openImage(src) {
+  const popup = document.getElementById("imagePopup");
+  const img = document.getElementById("popupImg");
+
+  popup.style.display = "flex";
+  img.src = src;   // replaces image (no extra image)
+}
+
+function closeImage() {
+  document.getElementById("imagePopup").style.display = "none";
+}
